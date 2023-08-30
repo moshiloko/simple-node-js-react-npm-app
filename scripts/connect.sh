5d1af0f3855fca07e44048ef48d1bbd618e4ee44
moshiloko <moshesamimi1999@gmail.com>
2023-08-30 13:32:16 +0300

    Update connect.sh

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,1 +1,2 @@
-sudo ssh -i /home/ubuntu/.ssh/authorized_keys ubuntu@ec2-18-221-243-67.us-east-2.compute.amazonaws.com
+#sudo ssh -i /home/ubuntu/.ssh/authorized_keys ubuntu@ec2-18-221-243-67.us-east-2.compute.amazonaws.com
+ls
